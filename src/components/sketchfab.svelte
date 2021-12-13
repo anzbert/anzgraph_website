43ab9c7346e9f039c711dc8008361c77b90a9c04
anzbert <38823700+anzbert@users.noreply.github.com>
2021-12-13 10:46:10 +0800

    sketchfab models

---
+++ src/components/sketchfab.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let caption: string = undefined;
+  export let src: string; // embed link, such as: "https://sketchfab.com/models/21de728eda6e4cc795468fb3ccfe835b/embed?ui_theme=dark"
+</script>
+
+<figure class="sketchfab-embed-wrapper">
+  <iframe
+    title="TH - Framing"
+    frameborder="0"
+    allowfullscreen
+    allow="autoplay; fullscreen; xr-spatial-tracking"
+    xr-spatial-tracking
+    execution-while-out-of-viewport
+    execution-while-not-rendered
+    web-share
+    {src}
+  />
+  {#if caption}
+    <figcaption>
+      {caption}
+    </figcaption>
+  {/if}
+</figure>
+
+<style>
+  iframe {
+    display: block;
+    width: min(100%, 500px);
+    height: 180px;
+  }
+</style>
