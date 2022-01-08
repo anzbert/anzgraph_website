dbe676ff1de905f334789ac85bebbbaf9ded9926
anzbert <anzbert@gmail.com>
2022-01-08 11:46:16 +0800

    added cat page

--- src/blog/!postindex.ts
+++ src/blog/!postindex.ts
@@ -12,6 +12,9 @@ function createPostObject(post: SvelteComponent, metadata: postMeta): post {
 export let allPosts: Array<post> = [];
 
 // Import posts (?change to dynamic import)
+import Post2022_01_08, { meta as meta2022_01_08 } from "../blog/2022-01-08.svx";
+allPosts.push(createPostObject(Post2022_01_08, meta2022_01_08));
+
 import Post2021_11_29, { meta as meta2021_11_29 } from "../blog/2021-11-29.svx";
 allPosts.push(createPostObject(Post2021_11_29, meta2021_11_29));
 
